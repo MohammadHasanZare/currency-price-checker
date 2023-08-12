a9518344b14b0e05c55e297bb7f4d365c8e5f390
mohammadhasan zare sheibani <108336902+MohammadHasanZare@users.noreply.github.com>
2023-08-12 20:31:41 +0330

    Update plugin-update-checker.php

--- plugin-update-checker-5.1/plugin-update-checker-5.1/plugin-update-checker.php
+++ plugin-update-checker-5.1/plugin-update-checker-5.1/plugin-update-checker.php
@@ -1,4 +1,13 @@
-<?php
+<?php 
+  require plugin_dir_path( __FILE__ ) . 'plugin-update-checker-5.1/plugin-update-checker.php';
+  
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+require 'E:\xamp\htdocs\theme\wp-content\plugins\currency-price-checker/plugin-update-checker-5.1/plugin-update-checker.php';
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/MohammadHasanZare/currency-price-checker/blob/main/plugin-update-checker-5.1/plugin-update-checker-5.1/examples/plugin.json',
+	__FILE__, //Full path to the main plugin file or functions.php.
+	'unique-plugin-or-theme-slug'
+);
 /**
  * Plugin Update Checker Library 5.1
  * http://w-shadow.com/
